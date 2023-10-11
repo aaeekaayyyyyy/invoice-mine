37e4924c78f0cea804507d1b33980de350d06731
aaeekaayyyyyy <ayushmankhandelwal04@gmail.com>
2023-10-11 12:44:30 +0530

    Creating register page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { ClothPageComponent } from './components/pages/cloth-page/cloth-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'cloth/:id', component: ClothPageComponent },
   { path: 'cart-page', component: CartPageComponent },
   { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
 ];
 
 @NgModule({
--- frontend/src/app/shared/constants/url.ts
+++ frontend/src/app/shared/constants/url.ts
@@ -5,3 +5,4 @@ export const CLOTHES_BY_SEARCH_URL = CLOTHES_URL + '/search/';
 export const CLOTHES_BY_ID_URL = CLOTHES_URL + '/';
 
 export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
+export const USER_REGISTER_URL = BASE_URL + '/api/users/register';
